87cd6add5e3cbfb08eaac86039cfdd1ec6d3d5b8
Vitaliy Voskolovych <v.voskolovych@ex.ua>
2016-06-08 16:34:48 +0300

    lessons3

--- lessons3/homework_3-3/script_3-3.js
+++ lessons3/homework_3-3/script_3-3.js
@@ -6,22 +6,16 @@ for (var i = 1; i < 101; i++) {
  
   n = "";
  
-  if (i%3 == 0 && i%5 == 0) {
-	  
-    n = n + "FizzBuzz";
-	
+  if (i%3 == 0 && i%5 == 0) {	  
+    n = n + "FizzBuzz";	
   } 
   
-  else if (i%3 == 0) {
-	  
-    n = n + "Fizz";
-	
+  else if (i%3 == 0) {	  
+    n = n + "Fizz";	
   } 
   
-  else if (i%5 == 0) {
-	  
-    n = n + "Buzz";
-	
+  else if (i%5 == 0) {	  
+    n = n + "Buzz";	
   }
   
   myArray.push(n || i);
